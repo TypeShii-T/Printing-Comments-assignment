5eb7b9f32fc6f21c4fd64b7708ad4df9d0f36f62
Liam Day <339710998@GoTVDSB.ca>
2024-09-06 09:13:39 -0400

    Add project files.

---
+++ Printing &Comments assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Printing__Comments_assignment
+{//Liam
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Goodafternoon Earth");   //Liam Day
+
+            Console.WriteLine("Comments are used to put names and comments for your work that doesnt go in when u run the program");
+            // I put spaces, dont worry
+            Console.WriteLine("L           D");
+            Console.WriteLine("L           D D");
+            Console.WriteLine("L           D  D");
+            Console.WriteLine("L           D   D");
+            Console.WriteLine("L           D   D");
+            Console.WriteLine("L           D  D");
+            Console.WriteLine("L           D D");
+            Console.WriteLine("L L L L L   D");
+
+            Console.ReadLine();
+            
+        }   
+    }
+}
